8451db7dfc1347d9631ca14417c3a03be73195e6
rick <github email address>
2020-02-26 23:22:01 -0500

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200210191952) do
+ActiveRecord::Schema.define(version: 20200210150215) do
 
   create_table "cars", force: :cascade do |t|
-    t.string "make"
-    t.string "model"
-    t.string "year"
+    t.string  "make"
+    t.string  "model"
+    t.string  "year"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
