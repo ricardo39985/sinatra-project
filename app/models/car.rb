de1af8582c1066fb60910220c7e0c5a68d33fa5e
rick <github email address>
2020-03-02 18:47:38 -0500

    Added min and max year for car year attribute

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,6 +3,9 @@ class Car < ActiveRecord::Base
   validates :year, format: { with: /(\d{4})/, message: "must be numbers" }
   validates :year, length: { is: 4 }, allow_blank: true
   validates :make, format:  {with: /[a-zA-Z]/, message: "must be only letters"}, allow_blank: true
+  validates_numericality_of :year, greater_than: 1885
+  validates_numericality_of :year, less_than: 2024
+
   
 
 end
